b6572d47cdd2bc841515a627b06fe68fafc336ab
UICMaster <ultrainstinctcrew.gaming@gmail.com>
2026-03-10 20:27:58 +0100

    Add files via upload

--- scripts/fetch_prime.js
+++ scripts/fetch_prime.js
@@ -16,7 +16,8 @@ const TEAMS = {
     "UIC_NOVA":    { id: "203447", manual_div: "Division 6" },
     "UIC_ABYSS":   { id: "204924", manual_div: "Division 5" },
     "UIC_NIGHT":   { id: "212047", manual_div: "Division 6" },
-    "UIC_FREEZER": { id: "203146", manual_div: "Division 5" }
+    "UIC_FREEZER": { id: "203146", manual_div: "Division 5" },
+    "RISING_DAWN": { id: "212208", manual_div: "Division 5" }
 };
 
 const HEADERS = { 'User-Agent': 'UIC-Dashboard-Bot/2.2' };
