1e26a74e8af86a44e87914e3d681a05984fdb514
UICMaster <ultrainstinctcrew.gaming@gmail.com>
2026-04-25 03:12:55 +0200

    Update fetch_prime.js

--- scripts/fetch_prime.js
+++ scripts/fetch_prime.js
@@ -11,7 +11,6 @@ const SEASON_START = new Date('2026-04-01T00:00:00');
 
 const TEAMS = {
     "RISING_DAWN": { id: "212208", manual_div: "Kalibrierung" },
-    "UIC_NOVA":    { id: "203447", manual_div: "Division 6" },
     "ODE_ABSOLUT_ZERO":   { id: "209281", manual_div: "Division 5" },
     "UIC_EMBER":   { id: "211165", manual_div: "Division 6" }
 };
