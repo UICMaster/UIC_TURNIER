3d16a99a377b40470d446b8b06d386f6907f34ca
UICMaster <ultrainstinctcrew.gaming@gmail.com>
2026-09-11 23:42:37 +0200

    Update fetch_prime.js

--- scripts/fetch_prime.js
+++ scripts/fetch_prime.js
@@ -12,7 +12,6 @@ const SEASON_START = new Date('2026-04-01T00:00:00');
 const TEAMS = {
     "UIC_CYBER":   { id: "198490", manual_div: "Division 6" },
     "UIC_SAKURA": { id: "208851", manual_div: "Division 5" },
-    "UIC_TEMPEST": { id: "208606", manual_div: "Division 5" },
     "GIG_LIZARDS": { id: "209324", manual_div: "Division 7" }
 };
 
